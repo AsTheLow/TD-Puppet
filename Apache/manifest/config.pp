c2c0f9d524341b86b2445592ee9ae96cbbdafddb
Théo <118971209+AsTheLow@users.noreply.github.com>
2023-06-09 23:23:52 +0200

    Update config.pp
    Add : class apache::config {
      case $operatingsystem {
        'Ubuntu', 'CentOS': {
          file { '/etc/apache2/apache2.conf':
            ensure  => present,
            owner   => 'root',
            group   => 'root',
            mode    => '0644',
            content => template('apache/default.conf.erb'),
            notify  => Service['apache2'],
          }
        }
        default: {
          fail("Unsupported operating system: ${operatingsystem}. Only Ubuntu and CentOS are supported.")
        }
      }
    }

--- Apache/manifest/config.pp
+++ Apache/manifest/config.pp
@@ -1,3 +1,21 @@
+class apache::config {
+  case $operatingsystem {
+    'Ubuntu', 'CentOS': {
+      file { '/etc/apache2/apache2.conf':
+        ensure  => present,
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0644',
+        content => template('apache/default.conf.erb'),
+        notify  => Service['apache2'],
+      }
+    }
+    default: {
+      fail("Unsupported operating system: ${operatingsystem}. Only Ubuntu and CentOS are supported.")
+    }
+  }
+}
+
 class apache_configuration::config {
   # Gestion du fichier de configuration Apache en utilisant un template
   
