f7edf7b6539f75922be031d6afa449ae5027f62f
Théo <118971209+AsTheLow@users.noreply.github.com>
2023-06-09 23:28:10 +0200

    Update config.pp
    class ssh_configuration::config {
      case $operatingsystem {
        'Ubuntu', 'CentOS': {
          # Managing SSH configuration file using a template
          file { '/etc/ssh/sshd_config':
            ensure  => file,
            content => template('ssh_configuration/sshd_config.erb'),
            require => Class['ssh_configuration::install'],
            notify  => Class['ssh_configuration::service'],
          }
        }
        default: {
          fail("Unsupported operating system: ${operatingsystem}. Only CentOS and Ubuntu are supported.")
        }
      }
    }

--- SSH/manifest/config.pp
+++ SSH/manifest/config.pp
@@ -1,10 +1,16 @@
 class ssh_configuration::config {
-  # Gestion du fichier de configuration SSH en utilisant un template
-  
-  file { '/etc/ssh/sshd_config':
-    ensure  => file,
-    content => template('ssh_configuration/sshd_config.erb'),
-    require => Class['ssh_configuration::install'],
-    notify  => Class['ssh_configuration::service'],
+  case $operatingsystem {
+    'Ubuntu', 'CentOS': {
+      # Managing SSH configuration file using a template
+      file { '/etc/ssh/sshd_config':
+        ensure  => file,
+        content => template('ssh_configuration/sshd_config.erb'),
+        require => Class['ssh_configuration::install'],
+        notify  => Class['ssh_configuration::service'],
+      }
+    }
+    default: {
+      fail("Unsupported operating system: ${operatingsystem}. Only CentOS and Ubuntu are supported.")
+    }
   }
 }
