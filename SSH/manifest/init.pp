46ff990d8b3afc38a374a0064558fe4f9f285277
Théo <118971209+AsTheLow@users.noreply.github.com>
2023-06-22 09:50:51 +0200

    Update init.pp

--- SSH/manifest/init.pp
+++ SSH/manifest/init.pp
@@ -1,10 +1,5 @@
-class ssh_configuration {
-  # Définition des dépendances entre les classes
-  Class['ssh_configuration::params']~>Class['ssh_configuration::config']~>Class['ssh_configuration::install']~>Class['ssh_configuration::service']
-  
-  # Inclusion des dépendances requises
-  include ssh_configuration::params
-  include ssh_configuration::config
-  include ssh_configuration::install
-  include ssh_configuration::service
+class ssh {
+  include ssh::install
+  include ssh::config
+  include ssh::service
 }
