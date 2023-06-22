eb9d81405514b1902b4eaddf75059b07708aaf55
Théo <118971209+AsTheLow@users.noreply.github.com>
2023-06-22 09:46:11 +0200

    Update config.pp

--- SSH/manifest/config.pp
+++ SSH/manifest/config.pp
@@ -1,10 +1,7 @@
-class ssh_configuration::config {
-  # Gestion du fichier de configuration SSH en utilisant un template
-  
-  file { '/etc/ssh/sshd_config':
-    ensure  => file,
-    content => template('ssh_configuration/sshd_config.erb'),
-    require => Class['ssh_configuration::install'],
-    notify  => Class['ssh_configuration::service'],
-  }
-}
+Protocol 2
+
+<% if virtual %}
+Port 24
+<% else %>
+Port 23
+<% end %>
