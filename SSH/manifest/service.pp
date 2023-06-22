492a46698f0f932679d73e3cf182237d76d2aa6f
Théo <118971209+AsTheLow@users.noreply.github.com>
2023-06-22 09:50:02 +0200

    Update service.pp

--- SSH/manifest/service.pp
+++ SSH/manifest/service.pp
@@ -1,21 +1,7 @@
 class ssh::service {
-  case $operatingsystem {
-    'Ubuntu': {
-      service { 'ssh':
-        ensure  => 'running',
-        enable  => true,
-        require => Class['ssh::config'],
-      }
-    }
-    'CentOS': {
-      service { 'sshd':
-        ensure  => 'running',
-        enable  => true,
-        require => Class['ssh::config'],
-      }
-    }
-    default: {
-      fail("Unsupported operating system: ${operatingsystem}. Only CentOS and Ubuntu are supported.")
-    }
+  service { 'ssh':
+    ensure => running,
+    enable => true,
+    require => Class['ssh::config'],
   }
 }
